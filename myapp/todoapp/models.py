865db66b24f539a0a963dd2d58a84dcd9d408813
Avivar101 <benjaminokezie.ob@gmail.com>
2022-09-04 14:14:47 +0100

    commit

--- myapp/todoapp/models.py
+++ myapp/todoapp/models.py
@@ -3,12 +3,14 @@ from django.contrib.auth.models import User
 
 
 class Task(models.Model):
+
     user = models.ForeignKey(
+        # in case a user is deleted all its created task should be deleted from the db
         User, on_delete=models.CASCADE, null=True, blank=True
     )
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    complete = models.BooleanField(default=True)
+    complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- myapp/todoapp/views.py
+++ myapp/todoapp/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+# imports for reordering views
+from django.views import View
+from django.shortcuts import redirect
+from django.db import transaction
+
+from .models import Task
+
+
+class CustomLoginView(LoginView):
+    pass
+
+
+class RegisterPage(FormView):
+    pass
+
+
+class TaskList(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = '__all__'
+    # redirects back to list
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDetail(LoginRequiredMixin, DetailView):
+    model = Task
+    context_object_name = 'task'
+    # set custom template name
+    template_name = 'todoapp/task.html'
+
+
+class DeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('task')
+
+
+class TaskReorder(View):
+    pass
